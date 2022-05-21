13253a437ad2178432afa576ad592ec470da0de6
mentorvikash <mentorvikash@gmail.com>
2022-05-21 15:01:48 +0530

    Add element to our actiion
    - Added a function so that when we click on add to cart button it will
    work as expected.
    
    Def: Action take the data bring to the storage created by redux.

---
+++ src/Services/Actions/action.js
@@ -0,0 +1,10 @@
+import { ADD_TO_CART, REMOVE_TO_CART } from "../constants"
+
+export const addToCart = (data) => {
+    return (
+        <>
+            type:ADD_TO_CART,
+            data: data
+        </>
+    )
+}
